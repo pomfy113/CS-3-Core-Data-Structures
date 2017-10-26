997469e81928418559dd8144aa8ae06afb60ad5c
pomfy113 <pomfy113@gmail.com>
2017-10-26 13:58:35 -0700

    Recursion done

--- source/recursion.py
+++ source/recursion.py
@@ -9,12 +9,19 @@ def factorial(n):
     # implement factorial_iterative and factorial_recursive below, then
     # change this to call your implementation to verify it passes all tests
     # return factorial_iterative(n)
-    return factorial_recursive(n)
+    return factorial_iterative(n)
 
 
 def factorial_iterative(n):
     # TODO: implement the factorial function iteratively here
-    pass
+    num = total = 1
+    while num != n+1:
+        total = total * num
+        num += 1
+        print(total)
+
+    return total
+
     # once implemented, change factorial (above) to call factorial_iterative
     # to verify that your iterative implementation passes all tests
 
