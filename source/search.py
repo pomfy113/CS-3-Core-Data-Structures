3277a993efa5d2c23acbbd4169ad44058ee4108d
pomfy113 <pomfy113@gmail.com>
2017-10-26 18:03:40 -0700

    Commented and cleaned up both functions

--- source/search.py
+++ source/search.py
@@ -17,12 +17,13 @@ def linear_search_iterative(array, item):
 
 
 def linear_search_recursive(array, item, index=0):
-    # TODO: implement linear search recursively here
+    # If you've gone too far, it's not in there
     if index >= len(array):
         return None
+    # If it matches, return index
     if array[index] == item:
         return index
-
+    # You uh... you add 1 to the index
     index += 1
     return linear_search_recursive(array, item, index)
 
@@ -40,72 +41,52 @@ def binary_search(array, item):
 
 def binary_search_iterative(array, item):
     # TODO: implement binary search iteratively here
+    # Starting off with left and right as margins
     left = 0
     right = len(array)-1
-    print("====================================")
-    print(array)
-    print("Looking for", item)
-
+    # Continue until return
     while True:
-        search = int((left+right) / 2)
-        # It's not pretty. I need to fix this.
-        print("Searching,", search, ". Left is", left, "right is", right)
-
-        if item == array[search]:
-            print("Found", item, "in", right)
-            return search
-        elif item == array[left]:
-            print("Found", item, "in", left)
-            return left
-        elif item == array[right]:
-            print("Found", item, "in", right)
-            return right
-
-        elif item > array[search]:
-            print("It's on the right side!\n\n")
-            left = search
-        elif item < array[search]:
-            print("It's on the left side!\n\n")
-            right = search
-
-        if (right-left) // 2 == 0:
-            print("Not found")
+        # Cut it in half
+        middle = left + ((right - left) // 2)
+        # Item found!
+        if item == array[middle]:
+            return middle
+        # If we're on the last two elements, check both if it matches
+        # Otherwise, it's not going to be in there
+        elif right - left <= 2:
+            if item == array[left]:
+                return left
+            elif item == array[right]:
+                return right
             return None
-
-    # once implemented, change binary_search to call binary_search_iterative
-    # to verify that your iterative implementation passes all tests
+        # Check if it's left or right of the middle
+        # If it's left, then we cut the right half and vice versa
+        elif item > array[middle]:
+            left = middle
+        elif item < array[middle]:
+            right = middle
 
 
 def binary_search_recursive(array, item, left=None, right=None):
-    # TODO: implement binary search recursively here
-    print(array)
+    # Only run at beginning
     if(left is None) and (right is None):
         left = 0
         right = len(array)-1
-    middle = (right - left) // 2
-    middleindex = left + middle
-    print("Searching for", item, "at index", middleindex, ".", "Left is:", left, ", right is:", right)
-    if item == array[left]:
-        print("Found at", left, "\n===========================")
-        return left
-    elif item == array[right]:
-        print("Found at", right, "\n===========================")
-        return right
-    elif item == array[middleindex]:
-        print("Found at", middleindex, "\n===========================")
+    # Index for the middle element
+    middleindex = left + ((right - left) // 2)
+    # If it's smack dab in the middle, return that
+    if item == array[middleindex]:
         return middleindex
-
-    elif(right-left) == 1:
-        print("Not here.")
+    # If it's down to the last two elements, check both
+    # If it's not in either, then it's not there
+    if right - left <= 2:
+        if item == array[left]:
+            return left
+        elif item == array[right]:
+            return right
         return None
-
+    # Otherwise, we check if it's left/right of middle and cut where it's not
     elif item > array[middleindex]:
-        return binary_search_recursive(array, item, left=middleindex, right=right)
+        return binary_search_recursive(array, item, middleindex, right)
     elif item < array[middleindex]:
-        return binary_search_recursive(array, item, left=left, right=middleindex)
-
-
-
-
-    # once implemented, change binary_search to call binary_search_recursive
-    # to verify that your recursive implementation passes all tests
+        return binary_search_recursive(array, item, left, middleindex)
